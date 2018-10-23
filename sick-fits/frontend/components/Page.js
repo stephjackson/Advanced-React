d3d6d0a497fdccb7718a77255e78edbaf40afef1
stephjackson <steph.garrett.jackson@gmail.com>
2018-10-23 14:42:31 -0400

    Adds styled component for a generic button

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
+import styled from 'styled-components';
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${props => props.huge ? '100px' : '50px'};
+  .yo {
+    font-size: 100px;
+  }
+`;
 
 class Page extends Component {
   render() {
@@ -8,6 +17,14 @@ class Page extends Component {
       <div>
         <Meta />
         <Header />
+        <MyButton huge>
+          Click Me
+          <span class="yo">Yo!</span>
+        </MyButton>
+        <MyButton>
+          Click Me
+          <span class="yo">Yo!</span>
+        </MyButton>
         <p>Hey, I'm the page component.</p>
         {this.props.children}
       </div>
