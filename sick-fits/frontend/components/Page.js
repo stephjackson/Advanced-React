19f97488fa5761cea96f36e7900282ec1305ac4a
stephjackson <steph.garrett.jackson@gmail.com>
2018-10-23 15:22:08 -0400

    Adds theme, begins styling for header

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Meta from './Meta';
-import styled from 'styled-components';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
-const MyButton = styled.button`
-  background: red;
-  font-size: ${props => props.huge ? '100px' : '50px'};
-  .yo {
-    font-size: 100px;
-  }
-`;
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+}
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 30px;
+`
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        <Meta />
-        <Header />
-        <MyButton huge>
-          Click Me
-          <span class="yo">Yo!</span>
-        </MyButton>
-        <MyButton>
-          Click Me
-          <span class="yo">Yo!</span>
-        </MyButton>
-        <p>Hey, I'm the page component.</p>
-        {this.props.children}
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>
+            {this.props.children}
+          </Inner>
+        </StyledPage>
+      </ThemeProvider>
     )
   }
 }
